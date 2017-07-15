e9a3657660b627dc1ae8c9baa33233a76a8bc478
Taillook <jousoku@gmail.com>
2017-07-15 14:05:34 +0900

    [Update] detection and talk

--- motion.py
+++ motion.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+import jtalk
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+
+try:
+        while True:        
+                if GPIO.input(18) == 1:
+			jtalk.jtalk("センサーだよ！")
+			sleep(15)
+
+except KeyboardInterrupt:
+        pass
+
+GPIO.cleanup()
