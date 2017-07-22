d50a6b5cd27c43a3738713078f6d17c3617dbe14
Taillook <jousoku@gmail.com>
2017-07-22 23:06:07 +0900

    [Update] sleep second

--- motion.py
+++ motion.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 from time import sleep
+import wave
 import jtalk
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 
 try:
-        while True:        
+        while True:
                 if GPIO.input(18) == 1:
 			jtalk.jtalk("センサーだよ！")
-			sleep(15)
+            wf = wave.open("open_jtalk.wav", "r")
+			sleep(float(wf.getnframes()) / wf.getframerate())
 
 except KeyboardInterrupt:
         pass
