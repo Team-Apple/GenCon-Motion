baf9a01b6b1e6505835c72e40eb7dd6b5a834f70
Kyoskk <s14543@salesio-sp.ac.jp>
2017-11-04 00:13:00 +0900

    API call and talk sentences

---
+++ make_sentence.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+
+def check_error(args):
+    modes = ['event', 's_task', 'b_task', 'c_task']
+    if not len(args) == 4:
+        print('Set three argments, mode(event, s_task, b_task, c_task), title and memo.')
+        sys.exit()
+    if not args[1] in modes:
+        print('Set first argment event, s_task, b_task or c_task.')
+        sys.exit()
+
+def get_file_message(path):
+    m_list = []
+    f = open(path, "r")
+    for line in f:
+        line = line.replace('\n', '')
+        m_list.append(line)
+    f.close()
+    random.shuffle(m_list)
+    return m_list[0]
+
+def make_sentence(mode, title, memo):
+    message = get_file_message(mode + '.txt')
+    out = message.replace('-', title.encode('utf_8'))
+    if not memo == '':
+        memorize = get_file_message('memo.txt')
+        memo_out = memorize.replace('-', memo.encode('utf_8'))
+        out += memo_out
+    return out
--- motion.py
+++ motion.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
-
 import RPi.GPIO as GPIO
 from time import sleep
 import wave
 import jtalk
+import sys
+import datetime
+import json
+import requests
+import make_sentence
+
+today = datetime.date.today()
+url = 'https://gencon-web.herokuapp.com/api/events.json?start_at_date=' + str(today)
+req = requests.get(url)
+event_dic = json.loads(req.text)
+print(event_dic)
+if event_dic == []:
+    print 'data is not exist.'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
@@ -11,10 +23,21 @@ GPIO.setup(18, GPIO.IN)
 try:
 	while True:
 		if GPIO.input(18) == 1:
-			jtalk.jtalk("センサーだよ！")
-			wf = wave.open("open_jtalk.wav", "r")
-			sleep(float(wf.getnframes()) / wf.getframerate())
-
+			mode = 'event'
+			today = datetime.date.today()
+			url = 'https://gencon-web.herokuapp.com/api/events.json?start_at_date=' + str(today)
+			req = requests.get(url)
+			event_dic = json.loads(req.text)
+			print(event_dic)
+			if event_dic == []:
+			    print 'data is not exist.'
+			for i in range(len(event_dic)):
+				title = event_dic[i]['title']
+				memo = event_dic[i]['memo']
+				sentence = make_sentence.make_sentence(mode, title, memo)
+				jtalk.jtalk(sentence)
+				wf = wave.open("open_jtalk.wav", "r")
+				sleep(float(wf.getnframes()) / wf.getframerate())
 except KeyboardInterrupt:
 	pass
 
